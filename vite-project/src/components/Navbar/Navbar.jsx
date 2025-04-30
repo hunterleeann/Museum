d0dae0a2184b77732273da1475ca4b7dffa142da
hunterleeann <hunterleeann41210@gmail.com>
2025-04-30 17:36:24 -0500

    added Navbar folder. Started Navbar definition

---
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Gallery</li>
+        <li>Login</li>
+        <li>Sign up</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
