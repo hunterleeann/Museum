cd5d5f5d8abbb8d12eab554078c4a75875497021
hunterleeann <hunterleeann41210@gmail.com>
2025-05-01 21:16:35 -0500

    Added some styling and store/api files

--- vite-project/src/components/Home/Home.jsx
+++ vite-project/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react' 
-import Navbar from '../Navbar/Navbar.jsx'
+import Navbar from '../Navbar/Navbar.jsx' 
+import './Home.css'
 
 const Home = () => {
   return (
--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import "./Navbar.css";
+import "./Navbar.css"; 
+import tdm from "/src/assets/tdm.jpg"
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar"> 
+    <img src={tdm} className="logo"/> 
       <ul>
         <li>Home</li>
         <li>Gallery</li>
         <li>Login</li>
-        <li>Sign up</li>
+        <li>Sign up</li> 
       </ul>
     </div>
   );
